e73a77fbeeca2adcceacda05c696ae73aab7000c
Ashley Schultz <ashleyeschultz@gmail.com>
2018-06-15 22:47:42 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -21,7 +21,6 @@ class ApplicationController < Sinatra::Base
     if params[:username] == ""
       redirect "/failure"
     elsif user.save
-        #session[:user_id] = user.id
         redirect "/login"
     else
         redirect "/failure"
